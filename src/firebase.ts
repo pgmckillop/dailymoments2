3957482546828aa597aa7111284868f12ebdf006
Paul McKillop <pgmckillop@gmail.com>
2020-09-20 14:10:25 +0100

    Lecture 60

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7-OpMIZN6XwmmtT7Z7s8VRngMmPN6Nkc",
+  authDomain: "daily-moments-193cb.firebaseapp.com",
+  databaseURL: "https://daily-moments-193cb.firebaseio.com",
+  projectId: "daily-moments-193cb",
+  storageBucket: "daily-moments-193cb.appspot.com",
+  messagingSenderId: "33647034283",
+  appId: "1:33647034283:web:27da1de8e6e0414166dcee",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,13 +2,18 @@ import {
   IonButton,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router";
 import { useAuth } from "../auth";
+import { auth } from "../firebase";
 
 interface Props {
   onLogin: () => void;
@@ -16,6 +21,14 @@ interface Props {
 
 const LoginPage: React.FC<Props> = ({ onLogin }) => {
   const { loggedIn } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const credential = await auth.signInWithEmailAndPassword(email, password);
+    onLogin();
+  };
+
   if (loggedIn) {
     return <Redirect to='/my/entries' />;
   }
@@ -28,7 +41,25 @@ const LoginPage: React.FC<Props> = ({ onLogin }) => {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-        <IonButton expand='block' onClick={onLogin}>
+        <IonList>
+          <IonItem>
+            <IonLabel position='stacked'>Email</IonLabel>
+            <IonInput
+              type='email'
+              value={email}
+              onIonChange={(event) => setEmail(event.detail.value)}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position='stacked'>Password</IonLabel>
+            <IonInput
+              type='password'
+              value={password}
+              onIonChange={(event) => setPassword(event.detail.value)}
+            />
+          </IonItem>
+        </IonList>
+        <IonButton expand='block' onClick={handleLogin}>
           Login
         </IonButton>
       </IonContent>
